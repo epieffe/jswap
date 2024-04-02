348629a904fde6943c3962a5de662fe18595f748
epieffe.eth <ferrariepifanio@gmail.com>
2024-04-02 10:31:04 +0200

    Silence usage on errors

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -17,7 +16,6 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
@@ -25,6 +23,7 @@ func Execute() {
 func init() {
 	rootCmd.CompletionOptions.HiddenDefaultCmd = true
 	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
+	rootCmd.SilenceUsage = true
 
 	rootCmd.SetUsageTemplate(USAGE_TEMPLATE)
 
